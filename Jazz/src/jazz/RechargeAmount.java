8609772f1e26c6763ac5d614eb8783c55af70c7a
Abdullah Yaqub <abdullahyaqub555@gmail.com>
2021-06-03 12:50:01 +0500

    Again

--- Jazz/src/jazz/RechargeAmount.java
+++ Jazz/src/jazz/RechargeAmount.java
@@ -10,5 +10,23 @@ package jazz;
  * @author it
  */
 public class RechargeAmount {
-    
+    int Balance;
+    Credit c=new Credit();
+    public boolean isBalanceVaild()
+    {
+        if(Balance>=0)
+        {
+            return true;
+        }
+    return false;
+    }
+    public boolean RechargeBalance()
+      
+    {
+        if(Balance<=c.Amount)
+        {
+            return true;
+        }
+        return false;
+    }
 }
